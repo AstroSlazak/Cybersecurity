e40a91d0796bc7f43c18283095bd671d334d88d5
Kamil <32820245+AstroSlazak@users.noreply.github.com>
2018-04-18 19:28:03 +0200

    Sieć neuronowa

--- Cybersecurity_TF.py
+++ Cybersecurity_TF.py
@@ -5,13 +5,16 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+import os
 
-name = 'capture20110810.csv'
+# Ścieżka dostępu do folderu z danymi
+Path = r"C:/Users/KamilM/Documents/GitHub/Cybersecurity/CTU-13-Dataset/"
+
 # Wczytanie danych
-def read_data():
-    df = pd.read_csv(name)
+def read_data(data_path):
+    df = pd.read_csv(data_path)
     # print(df.head())
-    X = df[['n_tcp','n_udp', 'n_icmp', 'background_flow_count', 'normal_flow_count', 'n_conn']].values
+    X = df[['avg_duration', 'n_dports>1024', 'n_dports<1024', 'n_tcp', 'n_icmp', 'n_udp', 'n_d_a_p_address', 'n_d_b_p_address', 'n_d_c_p_address', 'n_d_na_p_address', 'n_s_a_p_address', 'n_s_b_p_address', 'n_s_c_p_address', 'n_s_na_p_address', 'n_sports>1024', 'n_sports<1024',  'background_flow_count', 'normal_flow_count', 'n_conn']].values
     y = df['label'].values
     # Encode - zamiana Normal i Attack na 01 lub 10
     encoder = LabelEncoder()
@@ -28,43 +31,6 @@ def one_hot_encode(labels):
     one_hot_encode[np.arange(n_labels), labels] = 1
     return one_hot_encode
 
-# Wywołanie funkcji
-X ,Y = read_data()
-# Tasowanie(mieszanie) bazy rzędami
-X, Y = shuffle(X, Y, random_state = 415)
-# Stworzenie danych testowych i ćwiczeniowych (20% bazy do testów)
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20 ,random_state = 101)
-# Sprawdzenie rozmiarów danych testowych i ćwiczeniowych
-# print(x_train.shape)
-# print(y_train.shape)
-# print(x_test.shape)
-
-# Zdefiniowanie ważnych parametrów dla uczenia
-learning_rate = 0.0001 #Krok uczenia
-trainig_epochs = 1000 #liczba iteracji
-cost_history = np.empty(shape=[1] ,dtype=float)
-n_dim = X.shape[1] #liczba wejść
-n_class = 2 #liczba wyjść
-
-# zdefiniowanie liczby "ukrytych warstw" i ich liczby neuronów
-n_hidden_1 = 128
-n_hidden_2 = 256
-n_hidden_3 = 512
-n_hidden_4 = 1024
-n_hidden_5 = 2048
-n_hidden_6 = 2048
-n_hidden_7 = 2048
-n_hidden_8 = 1024
-n_hidden_9 = 512
-n_hidden_10 = 256
-n_hidden_11 = 128
-n_hidden_12 = 64
-
-# zdefiniowanie miejsca dla wejścia wyjścia bias'a i wagi
-x = tf.placeholder(tf.float32, [None, n_dim]) # Wejście0o  b
-W = tf.Variable(tf.zeros([n_dim, n_class])) # Wagi
-b = tf.Variable(tf.zeros([n_class])) # Bias
-y_ = tf.placeholder(tf.float32, [None, n_class]) # Wyjście
 
 # Zdefiniowanie modelu
 def multilayer_perceptron(x, weights, biases):
@@ -77,13 +43,14 @@ def multilayer_perceptron(x, weights, biases):
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.sigmoid(layer_2)
 
-    # Warstwa 3 z aktywacją sigmoid
+    # Warstwa 3 z aktywacją sigmoid , wejście to wyjście warstwy 2 x----> layer_2 , z nowymi wagami i bias'em
     layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
     layer_3 = tf.nn.sigmoid(layer_3)
 
-    # Warstwa 4 z aktywacją sigmoid
+    # Warstwa 4 z aktywacją sigmoid , wejście to wyjście warstwy 3 x----> layer_3 , z nowymi wagami i bias'em
     layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
     layer_4 = tf.nn.sigmoid(layer_4)
+
     # Warstwa 5 z aktywacją sigmoid
     layer_5 = tf.add(tf.matmul(layer_4, weights['h5']), biases['b5'])
     layer_5 = tf.nn.sigmoid(layer_5)
@@ -100,95 +67,118 @@ def multilayer_perceptron(x, weights, biases):
     layer_8 = tf.add(tf.matmul(layer_7, weights['h8']), biases['b8'])
     layer_8 = tf.nn.sigmoid(layer_8)
 
-    # Warstwa 9 z aktywacją sigmoid
-    layer_9 = tf.add(tf.matmul(layer_8, weights['h9']), biases['b9'])
-    layer_9 = tf.nn.sigmoid(layer_9)
-
-    # Warstwa 10 z aktywacją sigmoid
-    layer_10 = tf.add(tf.matmul(layer_9, weights['h10']), biases['b10'])
-    layer_10 = tf.nn.sigmoid(layer_10)
-
-    # Warstwa 11 z aktywacją sigmoid
-    layer_11 = tf.add(tf.matmul(layer_10, weights['h11']), biases['b11'])
-    layer_11 = tf.nn.sigmoid(layer_11)
-
-    # Warstwa 12 z aktywacją RELU
-    layer_12 = tf.add(tf.matmul(layer_11, weights['h12']), biases['b12'])
-    layer_12 = tf.nn.relu(layer_12)
-
     # warstwa wyjściowa z nowymi wagami i bias'em
-    out_layer = tf.matmul(layer_12, weights['out']) + biases['out']
+    out_layer = tf.matmul(layer_8, weights['out']) + biases['out']
     return out_layer
 
-# Zdefiniowanie wag i bias'a dla każdej warstwy (jako słownik)
-weights = {
-    'h1': tf.Variable(tf.random_normal([n_dim, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
-    'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4])),
-    'h5': tf.Variable(tf.random_normal([n_hidden_4, n_hidden_5])),
-    'h6': tf.Variable(tf.random_normal([n_hidden_5, n_hidden_6])),
-    'h7': tf.Variable(tf.random_normal([n_hidden_6, n_hidden_7])),
-    'h8': tf.Variable(tf.random_normal([n_hidden_7, n_hidden_8])),
-    'h9': tf.Variable(tf.random_normal([n_hidden_8, n_hidden_9])),
-    'h10': tf.Variable(tf.random_normal([n_hidden_9, n_hidden_10])),
-    'h11': tf.Variable(tf.random_normal([n_hidden_10, n_hidden_11])),
-    'h12': tf.Variable(tf.random_normal([n_hidden_11, n_hidden_12])),
-    'out': tf.Variable(tf.random_normal([n_hidden_12, n_class]))
-}
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
-    'b4': tf.Variable(tf.random_normal([n_hidden_4])),
-    'b5': tf.Variable(tf.random_normal([n_hidden_5])),
-    'b6': tf.Variable(tf.random_normal([n_hidden_6])),
-    'b7': tf.Variable(tf.random_normal([n_hidden_7])),
-    'b8': tf.Variable(tf.random_normal([n_hidden_8])),
-    'b9': tf.Variable(tf.random_normal([n_hidden_9])),
-    'b10': tf.Variable(tf.random_normal([n_hidden_10])),
-    'b11': tf.Variable(tf.random_normal([n_hidden_11])),
-    'b12': tf.Variable(tf.random_normal([n_hidden_12])),
-    'out': tf.Variable(tf.random_normal([n_class]))
-}
+# Utworzenie notatnika do zazpisania skuteczności
+with open("TF_accuracy.txt", "w") as output:
+    # Przeiterowanie przez wszystkie foldery z danymi
+    for n in range(1,14):
+        filelist = os.listdir(Path+str(n))
+        for i in filelist:
+            if i.endswith(".csv"):
+                data_path = Path+str(n)+"/"+i
+                print(data_path)
 
-# Wywołanie modelu
-y = multilayer_perceptron(x, weights, biases)
+                # Wywołanie funkcji
+                X ,Y = read_data(data_path)
+                # Tasowanie(mieszanie) bazy rzędami
+                X, Y = shuffle(X, Y, random_state = 415)
+                # Stworzenie danych testowych i ćwiczeniowych (30% bazy do testów)
+                x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.30 ,random_state = 101)
+                # Sprawdzenie rozmiarów danych testowych i ćwiczeniowych
+                # print(x_train.shape)
+                # print(y_train.shape)
+                # print(x_test.shape)
+                tf.reset_default_graph()
+                # zdefiniowanie liczby "ukrytych warstw" i ich liczby neuronów
+                n_hidden_1 = 128
+                n_hidden_2 = 256
+                n_hidden_3 = 256
+                n_hidden_4 = 512
+                n_hidden_5 = 512
+                n_hidden_6 = 256
+                n_hidden_7 = 256
+                n_hidden_8 = 128
 
-# zdefiniowanie odchylenia(strat) i optymalizacji
-cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-training_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost_function)
+                # Zdefiniowanie ważnych parametrów dla uczenia
+                learning_rate = 0.0001 #Krok uczenia
+                trainig_epochs = 500 #liczba iteracji
+                cost_history = np.empty(shape=[1] ,dtype=float)
+                n_dim = X.shape[1] #liczba wejść
+                n_class = 2 #liczba wyjść
 
-# Zainicjowanie wszystkich zmiennych
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
+                # zdefiniowanie miejsca dla wejścia wyjścia bias'a i wagi
+                x = tf.placeholder(tf.float32, [None, n_dim]) # Wejście0o  b
+                W = tf.Variable(tf.zeros([n_dim, n_class])) # Wagi
+                b = tf.Variable(tf.zeros([n_class])) # Bias
+                y_ = tf.placeholder(tf.float32, [None, n_class]) # Wyjście
 
-# Obliczenia odchylenia(strat) i skuteczność dla każdej iteracji
-mse_history = []
-accuracy_history = []
+                # Zdefiniowanie wag i bias'a dla każdej warstwy (jako słownik)
+                weights = {
+                    'h1': tf.Variable(tf.random_normal([n_dim, n_hidden_1])),
+                    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+                    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
+                    'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4])),
+                    'h5': tf.Variable(tf.random_normal([n_hidden_4, n_hidden_5])),
+                    'h6': tf.Variable(tf.random_normal([n_hidden_5, n_hidden_6])),
+                    'h7': tf.Variable(tf.random_normal([n_hidden_6, n_hidden_7])),
+                    'h8': tf.Variable(tf.random_normal([n_hidden_7, n_hidden_8])),
+                    'out': tf.Variable(tf.random_normal([n_hidden_8, n_class]))
+                }
+                biases = {
+                    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+                    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+                    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+                    'b4': tf.Variable(tf.random_normal([n_hidden_4])),
+                    'b5': tf.Variable(tf.random_normal([n_hidden_5])),
+                    'b6': tf.Variable(tf.random_normal([n_hidden_6])),
+                    'b7': tf.Variable(tf.random_normal([n_hidden_7])),
+                    'b8': tf.Variable(tf.random_normal([n_hidden_8])),
+                    'out': tf.Variable(tf.random_normal([n_class]))
+                }
 
-for epoch in range(trainig_epochs):
-    sess.run(training_step, feed_dict={x: x_train, y_: y_train})
-    cost = sess.run(cost_function, feed_dict={x: x_train, y_: y_train})
-    cost_history = np.append(cost_history, cost)
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    # print("Accuracy: ", (sess.run(accuracy, feed_dict={x: x_train, y_: y_train})))
-    pred_y = sess.run(y, feed_dict={x: x_test})
-    mse = tf.reduce_mean(tf.square(pred_y - y_test))
-    mse_ = sess.run(mse)
-    mse_history.append(mse_)
-    accuracy = sess.run(accuracy, feed_dict={x: x_train, y_: y_train})
-    accuracy_history.append(accuracy)
+                # Wywołanie modelu
+                y = multilayer_perceptron(x, weights, biases)
 
-    print('epoch: ', epoch, ' - ', 'cost: ', cost, "- MSE: ", mse_ , " - Train Accuracy: ", accuracy)
-# Zapis modelu sieci
-saver = tf.train.Saver()
-saver.save(sess, r'C:\Users\KamilM\Documents\GitHub\Cybersecurity\Model_TF\{}.ckpt'.format(name[:-4]))
+                # zdefiniowanie odchylenia(strat) i optymalizacji
+                cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+                training_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost_function)
 
-# Skuteczność
-plt.plot(accuracy_history)
-plt.xlabel('Ilość iteracji')
-plt.ylabel('Skuteczność')
-plt.savefig('Model_tensorflow_{}png'.format(name[:-3]),bbox_inches='tight', dpi=300)
+                # Zainicjowanie wszystkich zmiennych
+                init = tf.global_variables_initializer()
+                sess = tf.Session()
+                sess.run(init)
+
+                # Obliczenia odchylenia(strat) i skuteczność dla każdej iteracji
+                mse_history = []
+                accuracy_history = []
+
+                for epoch in range(trainig_epochs):
+                    sess.run(training_step, feed_dict={x: x_train, y_: y_train})
+                    cost = sess.run(cost_function, feed_dict={x: x_train, y_: y_train})
+                    cost_history = np.append(cost_history, cost)
+                    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+                    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                    # print("Accuracy: ", (sess.run(accuracy, feed_dict={x: x_train, y_: y_train})))
+                    pred_y = sess.run(y, feed_dict={x: x_test})
+                    mse = tf.reduce_mean(tf.square(pred_y - y_test))
+                    mse_ = sess.run(mse)
+                    mse_history.append(mse_)
+                    accuracy = sess.run(accuracy, feed_dict={x: x_train, y_: y_train})
+                    accuracy_history.append(accuracy)
+
+                    print('epoch: ', epoch, ' - ', 'cost: ', cost, "- MSE: ", mse_ , " - Train Accuracy: ", accuracy)
+                # Zapis modelu sieci
+                saver = tf.train.Saver()
+                saver.save(sess, r'C:\Users\KamilM\Documents\GitHub\Cybersecurity\Model_TF\{}.ckpt'.format(i[:-4]))
+                # zapisanie skuteczność do notatnika
+                output.write("Skuteczność {} wynosi {} \n".format(n,accuracy_history[-1]))
+
+                # Stworzenie wykresu skuteczności od iteracji
+                plt.plot(accuracy_history,label="Przypadek nr {} ".format(n))
+                plt.xlabel('Ilość iteracji')
+                plt.ylabel('Skuteczność')
+                plt.legend(bbox_to_anchor=(1.04,1), borderaxespad=0)
+plt.savefig('Model_tensorflow_{}.png'.format(learning_rate),bbox_inches='tight', dpi=300)
